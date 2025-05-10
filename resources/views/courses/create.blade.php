fbc515ca74f5ea942533687ebba0dd22aa521abc
Anggunsafitri22 <Anggun00iuu@gmail.com>
2025-05-10 10:38:20 +0700

    Part 10

--- resources/views/courses/create.blade.php
+++ resources/views/courses/create.blade.php
@@ -2,21 +2,17 @@
 @section('content')
  
 <div class="card">
-  <div class="card-header">Enrollment Page</div>
+  <div class="card-header">Courses Page</div>
   <div class="card-body">
       
-      <form action="{{ url('enrollments') }}" method="post">
+      <form action="{{ url('courses') }}" method="post">
         {!! csrf_field() !!}
-        <label>Enroll No</label></br>
-        <input type="text" name="enroll_no" id="enroll_no" class="form-control"></br>
-        <label>Batch ID</label></br>
-        <input type="text" name="batch_id" id="batch_id" class="form-control"></br>
-        <label>Student ID</label></br>
-        <input type="text" name="student_id" id="student_id" class="form-control"></br>
-        <label>Join Date</label></br>
-        <input type="text" name="join_date" id="join_date" class="form-control"></br>
-        <label>Fee</label></br>
-        <input type="text" name="fee" id="fee" class="form-control"></br>
+        <label>Name</label></br>
+        <input type="text" name="name" id="name" class="form-control"></br>
+        <label>Syllabus</label></br>
+        <input type="text" name="syllabus" id="syllabus" class="form-control"></br>
+        <label>Duration</label></br>
+        <input type="text" name="duration" id="duration" class="form-control"></br>
         <input type="submit" value="Save" class="btn btn-success"></br>
     </form>
    
